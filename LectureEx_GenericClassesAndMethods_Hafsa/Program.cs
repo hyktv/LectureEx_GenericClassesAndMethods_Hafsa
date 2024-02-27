cc51559c8b011fe9bc368323c1cd2145d0a23254
hyktv <hafsa.yktv@gmail.com>
2024-02-26 23:50:49 -0800

    Add project files.

---
+++ LectureEx_GenericClassesAndMethods_Hafsa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LectureEx_GenericClassesAndMethods_Hafsa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
